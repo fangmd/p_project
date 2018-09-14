3db9517833294aaeaea84d0148d65b5b1519ae3d
fangmingdong <fangmingdong@lianhejinxin.com>
2018-09-14 18:02:01 +0800

    router add pass parameters

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -11,7 +11,16 @@ Map<String, WidgetBuilder> buildRoutes2() {
 
 ///路由管理
 class RouterUtils {
+  ///暂时不能传参数
   static void pushNamed(BuildContext context, String routeName) {
     Navigator.pushNamed(context, routeName);
+  }
+
+  /// push 可以传参数
+  static void push(BuildContext context, Widget page) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (_) => page),
+    );
   }
 }
