6aa6354db71288b0acc438cf643d5519f1a52227
fangmingdong <fangmingdong@lianhejinxin.com>
2018-09-17 11:35:05 +0800

    add style

---
+++ lib/style/my_styles.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyStyleUtils {
+  static const lagerTextSize = 30.0;
+  static const bigTextSize = 23.0;
+  static const normalTextSize = 18.0;
+  static const middleTextWhiteSize = 16.0;
+  static const smallTextSize = 14.0;
+  static const minTextSize = 12.0;
+
+  static const normalSubText = TextStyle(
+    color: Colors.lightGreen,
+    fontSize: normalTextSize,
+  );
+}
