3b75ac38636b4b772ef025625c6c83464f3bf435
fangmingdong <fangmingdong@lianhejinxin.com>
2018-09-18 10:02:51 +0800

    add appbar

---
+++ lib/common/widgets/my_app_bar.dart
@@ -0,0 +1,21 @@
+//import 'package:flutter/material.dart';
+//
+//class MyAppBar extends StatefulWidget implements PreferredSizeWidget {
+//  @override
+//  State<StatefulWidget> createState() {
+//    return _MyAppBarState();
+//  }
+//
+//  @override
+//  final Size preferredSize;
+//
+//}
+//
+//class _MyAppBarState extends State<MyAppBar> {
+//  @override
+//  Widget build(BuildContext context) {
+//    return Center(
+//      child: Text('title'),
+//    );
+//  }
+//}
